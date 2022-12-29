bb17e8f072dc6d9fc4c72309f3118e094c91eed8
Abdullah Mzaien <xa300600@hotmail.com>
2022-12-29 23:56:54 +0300

    chore: update mitosis

--- output/angular/input.ts
+++ output/angular/input.ts
@@ -1,5 +1,5 @@
 import { NgModule } from "@angular/core";
-import { BrowserModule } from "@angular/platform-browser";
+import { CommonModule } from "@angular/common";
 
 import { Component, Input } from "@angular/core";
 
@@ -42,7 +42,7 @@ export class MyComponent {
 
 @NgModule({
   declarations: [MyComponent],
-  imports: [BrowserModule],
+  imports: [CommonModule],
   exports: [MyComponent],
 })
 export class MyComponentModule {}
