05f9d0056779606e80cfe5013af545bf60c371b5
Abdullah Mzaien <xa300600@hotmail.com>
2023-01-27 19:20:36 +0300

    chore: update deps

--- docs/astro.config.mjs
+++ docs/astro.config.mjs
@@ -1,4 +1,5 @@
 import { defineConfig } from 'astro/config';
+import vercel from '@astrojs/vercel/edge';
 import preact from '@astrojs/preact';
 import react from '@astrojs/react';
 
@@ -19,8 +20,19 @@ import mdx from "@astrojs/mdx";
 // https://astro.build/config
 export default defineConfig({
   integrations: [
-  // Enable Preact to support Preact JSX components.
-  preact(),
-  react(), solidJs(), svelte(), vue(), lit(), alpinejs(), mdx()],
-  site: `http://astro.build`
+    // Enable Preact to support Preact JSX components.
+    preact(),
+    react(),
+    solidJs(),
+    svelte(),
+    vue(),
+    lit(),
+    alpinejs(),
+    mdx({
+      extendMarkdownConfig: false,
+      smartypants: true,
+      gfm: true,
+    }),
+  ],
+  site: `https://arabic-to-english-input.vercel.app/`,
 });
